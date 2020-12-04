c263bbeb2dfd293cfe7aa466f2ce71e7e94b32ed
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2020-12-04 16:23:44 +0300

    Initialize some files

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: "module"
+	},
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"no-var": "warn",
+		"prefer-const": "warn",
+		"@typescript-eslint/explicit-function-return-type": "warn",
+		"@typescript-eslint/no-non-null-assertion": "error",
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{
+				vars: "all",
+				args: "after-used",
+				varsIgnorePattern: "^_",
+				argsIgnorePattern: "^_"
+			}
+		],
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"]
+	}
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: "none",
+    singleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: "lf",
+    useTabs: true
+};
