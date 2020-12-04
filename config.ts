35eb9016810100653b80babeedaf7764d0ff71e8
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2020-12-04 21:50:18 +0300

    Change dotenv with local config & local config loader

---
+++ config.ts
@@ -0,0 +1,20 @@
+import * as fs from "fs";
+import * as path from "path";
+
+const CONFIG = {
+	DISCORD_TOKEN: "PUT_YOUR_TOKEN_HERE_OR_CREATE_'config.local.ts'_AND_EDIT_IT_THERE",
+	commandPrefix: "!",
+	reportServerId:
+		"YOU_CAN_GET_THIS_BY_RIGHT_CLICKING_TO_SERVER_YOU_WANT_TO_USE_AFTER_ENABLING_DEVELOPER_MODE_ON_DISCORD",
+	reportChannelId:
+		"YOU_CAN_GET_THIS_BY_RIGHT_CLICKING_TO_CHAT_YOU_WANT_TO_USE_AFTER_ENABLING_DEVELOPER_MODE_ON_DISCORD",
+	developerUserId: "YOU_CAN_GET_THIS_BY_RIGHT_CLICKING_TO_YOURSELF_AFTER_ENABLING_DEVELOPER_MODE_ON_DISCORD"
+};
+
+const localConfigPath = path.join(__dirname, "config.local.ts");
+if (fs.existsSync(localConfigPath)) {
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	Object.assign(CONFIG, require(localConfigPath));
+}
+
+export default CONFIG;
